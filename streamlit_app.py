d1cd0564320895845d1532ab4c1b9904300f05ae
snowflakeconner <111015107+snowflakeconner@users.noreply.github.com>
2022-08-11 23:41:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -35,6 +36,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
