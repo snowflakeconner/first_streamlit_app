c6bef4c566636e430dc60b52b0ce46edc970f379
snowflakeconner <111015107+snowflakeconner@users.noreply.github.com>
2022-08-12 00:09:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
